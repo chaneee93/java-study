87396b0a79cb8661866498badf196b73dfba4d79
chaneee93 <chaneee93@google.com>
2026-09-07 14:07:49 +0900

    feat: try-catch 실습 및 예외 처리

--- day05/src/Main.java
+++ day05/src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int[] a = new int[3];   // 0,1,2번 칸만 있음
-        a[5] = 1;               // 5번 칸? 없음!
+        try {
+            System.out.println("try");
+            int x = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("catch");
+        } finally {
+            System.out.println("finally");
+        }
     }
 }
